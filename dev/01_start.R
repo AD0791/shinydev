deafe7c7dfe9e50fdb380ae95bab69f410bb5062
AD0791 <alexdisla1991@gmail.com>
2024-07-14 11:52:23 -0400

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -21,7 +21,7 @@
 golem::fill_desc(
   pkg_name = "shinydev", # The Name of the package containing the App
   pkg_title = "ShinyDev first application", # The Title of the package containing the App
-  pkg_description = "A basic use of the framework", # The Description of the package containing the App
+  pkg_description = "A basic use of the framework and the shiny.", # The Description of the package containing the App
   author_first_name = "Alexandro", # Your First Name
   author_last_name = "Disla", # Your Last Name
   author_email = "alexdisla1991@gmail.com", # Your Email
@@ -37,12 +37,12 @@ golem::install_dev_deps()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_mit_license("Alexandro Disla") # You can set another license here
 usethis::use_readme_rmd(open = FALSE)
 devtools::build_readme()
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
+usethis::use_code_of_conduct(contact = "Alexandro Disla")
 usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
 
